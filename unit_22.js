319af942d18873f4cc366e963f1e11e194d1c7f1
Vlad Ivashchenko <mr.ruryks@gmail.com>
2020-03-09 17:42:39 +0200

    Unit22

---
+++ unit_22.js
@@ -0,0 +1,129 @@
+
+// Task 1 ============================================
+/* Создайте блок div-1. Добавьте на него событие onclick. При срабатывании события  выводите в out-1 количество срабатываний события. В комментариях опишите результат. */
+
+let count1 = 0;
+
+function t1() {
+    count1++;
+    document.querySelector('.out-1').innerHTML = count1;
+}
+
+document.querySelector('.div-1').onclick = t1;
+
+// Task 2 ============================================
+/* Создайте блок div-2. Добавьте на него событие ondblclick. При срабатывании события  выводите в out-2 количество срабатываний события. В комментариях опишите результат. */
+
+let count2 = 0;
+
+function t2() {
+    count2++;
+    document.querySelector('.out-2').innerHTML = count2;
+}
+
+document.querySelector('.div-2').ondblclick = t2;
+
+
+// Task 3 ============================================
+/*  Создайте блок div-3. Добавьте на него событие onmousemove. При срабатывании события выводите в out-3 количество срабатываний события. В комментариях опишите результат. */
+
+let count3 = 0;
+
+function t3() {
+    count3++;
+    document.querySelector('.out-3').innerHTML = count3;
+}
+
+document.querySelector('.div-3').onmousemove = t3;
+
+
+// Task 4 ============================================
+/*  Создайте блок div-4. Добавьте на него событие oncontextmenu. При срабатывании события выводите в out-4 количество срабатываний события. В комментариях опишите результат. */
+
+let count4 = 0;
+
+function t4() {
+    count4++;
+    document.querySelector('.out-4').innerHTML = count4;
+}
+
+document.querySelector('.div-4').oncontextmenu = t4;
+
+// Task 5 ============================================
+/*   Создайте блок div-5. Добавьте на него событие onmousedown. При срабатывании события выводите в out-5 количество срабатываний события. В комментариях опишите результат. */
+
+let count5 = 0;
+
+function t5() {
+    count5++;
+    document.querySelector('.out-5').innerHTML = count5;
+}
+
+document.querySelector('.div-5').onmousedown = t5;
+
+// Task 6 ============================================
+/*  Создайте блок div-6. Добавьте на него событие onmouseenter. При срабатывании события выводите в out-6 количество срабатываний события. В комментариях опишите результат. */
+
+let count6 = 0;
+
+function t6() {
+    count6++;
+    document.querySelector('.out-6').innerHTML = count6;
+}
+
+document.querySelector('.div-6').onmouseenter = t6;
+
+
+// Task 7 ============================================
+/*   Создайте блок div-7. Добавьте на него событие onmouseleave. При срабатывании события выводите в out-7 количество срабатываний события. В комментариях опишите результат.*/
+
+let count7 = 0;
+
+function t7() {
+    count7++;
+    document.querySelector('.out-7').innerHTML = count7;
+}
+
+document.querySelector('.div-7').onmouseleave = t7;
+
+// Task 8 ============================================
+/*   Создайте блок div-8. Добавьте на него событие onmouseout. При срабатывании события выводите в out-8 количество срабатываний события. В комментариях опишите результат.*/
+
+let count8 = 0;
+
+function t8() {
+    count8++;
+    document.querySelector('.out-8').innerHTML = count8;
+}
+
+document.querySelector('.div-8').onmouseout = t8;
+
+
+// Task 9 ============================================
+/* Создайте блок div-9. Добавьте на него событие onmouseover. При срабатывании события выводите в out-9 количество срабатываний события. В комментариях опишите результат. */
+
+let count9 = 0;
+
+function t9() {
+    count9++;
+    document.querySelector('.out-9').innerHTML = count9;
+}
+
+document.querySelector('.div-9').onmouseover = t9;
+
+
+// Task 10 ============================================
+/*  Создайте блок div-10. Добавьте на него событие onmouseup. При срабатывании события выводите в out-10 количество срабатываний события. В комментариях опишите результат.*/
+
+let count10 = 0;
+
+function t10() {
+    count10++;
+    document.querySelector('.out-10').innerHTML = count10;
+}
+
+document.querySelector('.div-10').onmouseup = t10;
+
+
+
+
